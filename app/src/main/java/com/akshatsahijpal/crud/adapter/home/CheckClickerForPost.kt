9686c39397390a581359d233ff45a420fc8b34cb
akshat <20akshatsahijpal01droid01@gmail.com>
2021-10-19 08:28:22 +0530

    Added OnClick Method on Post

--- app/src/main/java/com/akshatsahijpal/crud/adapter/home/CheckClickerForPost.kt
+++ app/src/main/java/com/akshatsahijpal/crud/adapter/home/CheckClickerForPost.kt
@@ -2,4 +2,4 @@ package com.akshatsahijpal.crud.adapter.home
 
 import com.akshatsahijpal.crud.data.PostFeedData
 
-data class CheckClickerForPost(var clickListener: (data: PostFeedData)->Unit)
+data class CheckClickerForPost(var clickListener: (data: PostFeedData) -> Unit)
