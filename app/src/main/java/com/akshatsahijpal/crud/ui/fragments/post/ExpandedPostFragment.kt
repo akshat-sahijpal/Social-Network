9f32f7eaafea7d5fbce9098259046fc9f56258c6
akshat <20akshatsahijpal01droid01@gmail.com>
2021-10-23 00:59:18 +0530

    Added OnClick Method on Post

--- app/src/main/java/com/akshatsahijpal/crud/ui/fragments/post/ExpandedPostFragment.kt
+++ app/src/main/java/com/akshatsahijpal/crud/ui/fragments/post/ExpandedPostFragment.kt
@@ -13,9 +13,12 @@ import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.akshatsahijpal.crud.adapter.comment.CommentRecyclerAdapter
 import com.akshatsahijpal.crud.data.CommentArchitectureData
+import com.akshatsahijpal.crud.data.CommentData
 import com.akshatsahijpal.crud.data.PostFeedData
 import com.akshatsahijpal.crud.databinding.ExpandedPostFragmentBinding
 import com.akshatsahijpal.crud.util.Constants
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
@@ -25,10 +28,12 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import java.util.*
 
 @AndroidEntryPoint
 class ExpandedPostFragment : Fragment() {
     private val args: ExpandedPostFragmentArgs by navArgs()
+
     private val model: ExpandedPostViewModel by viewModels()
     private lateinit var _binding: ExpandedPostFragmentBinding
     private var adapter = CommentRecyclerAdapter(
@@ -110,6 +115,18 @@ class ExpandedPostFragment : Fragment() {
                 }
                 if (data.postProfilePicture == null) {
                     Picasso.get().load(Constants.DefaultProfilePhoto).into(mainScope.profilePicture)
+                }
+                commentScope.CommentButtonForPost.setOnClickListener {
+                    if(!commentScope.mainCommentContentForThePost.text.equals(" ")){
+                        val account = GoogleSignIn.getLastSignedInAccount(requireContext())
+                        val data = CommentData(account.photoUrl.toString(),Calendar.getInstance().time.toString(),account.displayName, commentScope.mainCommentContentForThePost.text.toString())
+                        model.commentUploader(data)
+                        model.listenCommentData().observe(viewLifecycleOwner){
+                            model.updatePostWithCommentRef(it, args.uid)
+                        }
+                        commentScope.mainCommentContentForThePost.setText("")
+                    }
+
                 }
                 CommentRecyclerView.adapter = adapter
                 CommentRecyclerView.layoutManager = LinearLayoutManager(requireContext())
